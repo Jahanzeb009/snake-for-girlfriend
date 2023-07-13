09cf466bf84355d3acf3ba76bd6d97723ec7b39b
Jahanzeb <zaibi4014@gmail.com>
2023-07-14 00:49:22 +0500

    app crash bug fixed

--- fullView.js
+++ fullView.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { SettingsContext } from './settings/SettingsContext';
-import SnakeApp from './index.js';
-import * as Font from 'expo-font';
+import SnakeApp from './snakeApp.js';
 
 import { useState, useEffect, useContext } from 'react';
+import { useFonts } from 'expo-font';
 
 export default function FullView(props) {
     const settings = useContext(SettingsContext);
     const [isReady, setIsReady] = useState(false);
 
-    useEffect(() => {
-        const loadFonts = async () => {
-            await Font.loadAsync({
-                "Billy": require('./assets/fonts/Billy/Billy-Regular.ttf'),
-                'Billy-Bold': require('./assets/fonts/Billy/Billy-Bold.ttf'),
-                'Billy-Light': require('./assets/fonts/Billy/Billy-Light.ttf'),
-            });
-            setIsReady(true);
-        };
-        loadFonts();
-    }, []);
-
     console.log('====================================');
     console.log(settings.theme, 'settings.theme....');
     console.log('====================================');
+
+    const [fontsLoaded] = useFonts({
+        "Billy": require('./assets/fonts/Billy/Billy-Regular.ttf'),
+        'Billy-Bold': require('./assets/fonts/Billy/Billy-Bold.ttf'),
+        'Billy-Light': require('./assets/fonts/Billy/Billy-Light.ttf')
+    })
+
+
+    useEffect(() => {
+        setIsReady(true)
+    }, [fontsLoaded])
+
+
+    if (!fontsLoaded) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: 'white' }}>Fonts's loading...</Text>
+            </View>
+        )
+    }
 
     return (
         <View
--- tail.js
+++ tail.js
@@ -1,35 +1,75 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import { SettingsContext } from './settings/SettingsContext';
+
+// class Tail extends Component {
+//     static contextType = SettingsContext;
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         const tailList = this.props.elements.map((pos, i) => {
+//             return (
+//                 <View
+//                     key={i}
+//                     style={[
+//                         styles.tail,
+//                         {
+//                             width: this.props.size,
+//                             height: this.props.size,
+//                             left: pos[0] * this.props.size,
+//                             top: pos[1] * this.props.size,
+//                             backgroundColor: this.context.theme.complementary2,
+//                             borderColor: this.context.theme.primary,
+//                         },
+//                     ]}
+//                 />
+//             );
+//         });
+
+//         return <View>{tailList}</View>;
+//     }
+// }
+
+// const styles = StyleSheet.create({
+//     tail: {
+//         position: 'absolute',
+//         borderWidth: 1,
+//     },
+// });
+
+// export { Tail };
+
+
+import React, { Component, useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SettingsContext } from './settings/SettingsContext';
 
-class Tail extends Component {
-    static contextType = SettingsContext;
-    constructor(props) {
-        super(props);
-    }
+const Tail = (props) => {
 
-    render() {
-        const tailList = this.props.elements.map((pos, i) => {
-            return (
-                <View
-                    key={i}
-                    style={[
-                        styles.tail,
-                        {
-                            width: this.props.size,
-                            height: this.props.size,
-                            left: pos[0] * this.props.size,
-                            top: pos[1] * this.props.size,
-                            backgroundColor: this.context.theme.complementary2,
-                            borderColor: this.context.theme.primary,
-                        },
-                    ]}
-                />
-            );
-        });
+    const context = useContext(SettingsContext);
 
-        return <View>{tailList}</View>;
-    }
+    const tailList = props.elements.map((pos, i) => {
+        return (
+            <View
+                key={i}
+                style={[
+                    styles.tail,
+                    {
+                        width: props.size,
+                        height: props.size,
+                        left: pos[0] * props.size,
+                        top: pos[1] * props.size,
+                        backgroundColor: context.theme.complementary2,
+                        borderColor: context.theme.primary,
+                    },
+                ]}
+            />
+        );
+    });
+
+    return <View>{tailList}</View>;
 }
 
 const styles = StyleSheet.create({
